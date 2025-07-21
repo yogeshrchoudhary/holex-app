18842d38eab7ca1fae0cd821d615fd12d56d5c56
Yogesh Choudhary <yogesh.choudhary@parentpay.com>
2025-07-21 08:20:54 +0100

    Banners and general layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from "./banner/banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'A blank app...';
 }
