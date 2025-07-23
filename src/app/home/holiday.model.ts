cc0e9cbf1550f1e65dd2f610a4ec59f9cb182a8d
Yogesh Choudhary <yogesh.choudhary@parentpay.com>
2025-07-23 05:36:13 +0100

    Some more cool templating and a list of past holidays

---
+++ src/app/home/holiday.model.ts
@@ -0,0 +1,7 @@
+export interface IHoliday {
+    id: number;
+    date: Date;
+    title: string;
+    description: string;
+    imageUrl?: string;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { IHoliday } from './holiday.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'hol-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  holiday: IHoliday = {
+    title: 'Dorset',
+    id: 1,
+    date: new Date('2023-12-25'),
+    description: 'Lulworth Cove, Dorset and the Jurassic Coast World Heritage Site'
+  }
 
+  holidays: IHoliday[] = [
+    {
+      title: 'Dorset',
+      id: 1,
+      date: new Date('2025-05-22'),
+      description: 'Lulworth Cove, Dorset and the Jurassic Coast World Heritage Site'
+    },  
+    {
+      title: 'Cotswolds',
+      id: 2,
+      date: new Date('2025-03-10'),
+      description: 'Lovely vilages of Bourton-on-the-Water, Cotswolds and Moreton-in-Marsh Market Town',
+    }];
+
+
+  generateLogoUrl(holiday: IHoliday): string {
+    return `https://picsum.photos/seed/${holiday.date.getDay()}/75/75`;
+  }
 }
